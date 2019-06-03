2e3283ab9d079d24e5e857153ece7591e11f15f9
Andrew Davisson <akdavisson4@gmail.com>
2019-06-03 00:41:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,12 +14,22 @@ const codes = [
 function init() {
   // your code here
 
+  let i = 0;
+  
   function konamiCode(e){
     const key = e.key;
     
-    if (key === codes[])
+    if (key === codes[i]){
+      i++
+      if(i === codes.length){
+        alert('Congrats');
+        i = 0;
+      }
+    }else{
+      i = 0;
+    }
   }
   
-  
+  document.body.addEventListener('keyDown', konamiCode);
   
 }
